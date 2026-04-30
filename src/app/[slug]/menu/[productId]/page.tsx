3ee6523e037eca8336a3eff468d4c1bbb5d22799
Vinicius Benedito <viniciusaugustobenedito@gmail.com>
2026-04-30 12:13:26 -0300

    feat: add product details page

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/app/_lib/prisma";
 
+import ProductDetails from "./components/product-details";
 import ProductHeader from "./components/product-header";
 
 interface ProductPageProps {
@@ -12,19 +13,30 @@ const ProductPage = async ({ params }: ProductPageProps) => {
   const { slug, productId } = await params;
   const product = await db.product.findUnique({
     where: { id: productId },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+          avatarImageUrl: true,
+          slug: true,
+        },
+      },
+    },
   });
 
   if (!product) {
     return notFound();
   }
 
+  if (product.restaurant.slug.toUpperCase() != slug.toUpperCase()) {
+    return notFound();
+  }
+
   return (
-    <>
+    <div className="flex h-full flex-col">
       <ProductHeader product={product} />
-      <h1>Product page</h1>
-      {slug}
-      {productId}
-    </>
+      <ProductDetails product={product} />
+    </div>
   );
 };
 
