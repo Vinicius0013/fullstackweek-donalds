b04efccf4efcb1703b88aab7fbb40f6c858caca8
Vinicius Benedito <viniciusaugustobenedito@gmail.com>
2026-06-19 15:14:44 -0300

    fix: ensure user is redirects with cpf after order creation

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
-import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 import { db } from "@/app/_lib/prisma";
 
@@ -43,7 +43,7 @@ export const createOrder = async (input: CreateOrderInput) => {
     price: productsWithPrices.find((p) => p.id === product.id)!.price,
   }));
 
-  const order = await db.order.create({
+  await db.order.create({
     data: {
       status: "PENDING",
       customerName: input.customerName,
@@ -61,13 +61,8 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
-  revalidatePath(
+
+  redirect(
     `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`,
   );
-
-  // redirect(
-  //   `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`,
-  // );
-
-  return order;
 };
